332dd36f1cceafea7f1885b64d62fa6db767aa2c
XxShayanxxX <164016339+XxShayanxxX@users.noreply.github.com>
2024-12-06 20:31:21 +0800

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,13 @@
+class  myclass :
+    x = 10 
+    __privateclass = 100 
+
+    def hello(self):
+        print("HELO GUYS :D")
+
+
+    def endclass(self):
+        print("endlclass",myclass.__privateclass)
+
+
+
